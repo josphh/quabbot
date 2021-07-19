0fb2da8a401b0bbe59472245c22db1f9160ca28e
Joseph Nuttall <josphh@protonmail.com>
2021-07-19 21:08:57 +0100

    It all finally works :sparkles

--- quabbot/main.py
+++ quabbot/main.py
@@ -1,4 +1,4 @@
-import json
+import jsons
 import os
 import random
 import sys
@@ -51,7 +51,7 @@ async def adopt(ctx):
     else:
         with open(f"./quabbot/users/{ctx.author.id}.json", "w") as file:
             name = generate_name()
-            jsons.dump({"name": name}, file)
+            file.write(jsons.dumps({"name": name, "timeCreated": datetime.datetime.now()}))
             await ctx.send(f"Quib adopted; Their name is {name}!")
 
 
@@ -73,9 +73,9 @@ async def disown(ctx):
 async def info(ctx):
     if os.path.exists(f"./quabbot/users/{ctx.author.id}.json"):
         with open(f"./quabbot/users/{ctx.author.id}.json", "r") as file:
-            data = json.load(file)
+            data = jsons.loads(file.read())
             name = data["name"]
-            embed = discord.Embed(title = name, description = 'stuff')
+            embed = discord.Embed(title = name, description = data["timeCreated"].strftime("%m/%d/%Y"))
             embed.set_image(url='https://raw.githubusercontent.com/josphh/quabbot/master/quabbot/resources/quib.png')
             await ctx.send(embed=embed)
     else:
